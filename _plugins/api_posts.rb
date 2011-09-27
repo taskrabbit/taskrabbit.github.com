f450c8c6ad63e744fc33e937baac716e7f87fa5a
jrichardlai <jrichardlai@gmail.com>
2011-09-27 15:35:58 -0700

    Add taskrabbit api documentation

---
+++ _plugins/api_posts.rb
@@ -0,0 +1,32 @@
+STDERR.puts 'Running to match api mdown files'
+
+module Jekyll
+  class Post
+    # A new way of recognising posts.
+    remove_const(:MATCHER)
+    MATCHER = /^(\d+)-(.*)(\.[^.]+)$/
+    attr_accessor :order
+
+    # Spaceship is based on Post#order, date, slug
+    #
+    # Returns -1, 0, 1
+    def <=>(other)
+      cmp = self.order <=> other.order
+      if 0 == cmp
+       cmp = super
+      end
+      return cmp
+    end
+
+    # Processing the posts.
+    def process(name)
+      m, order, slug, ext = *name.match(MATCHER)
+      self.date  = Time.now
+      self.order = order
+      self.slug  = slug
+      self.ext   = ext
+    rescue ArgumentError
+      raise FatalException.new("Post #{name} does not have a order number.")
+    end
+  end
+end
